a1ffec5e4674f645cb85a95b5b72b52ad37e997d
Jesper <karnich99@gmail.com>
2014-11-19 17:58:44 +0100

    parameters update

--- Implementering/SW/PSoC/Parameters/Parameters/parameter_test.c
+++ Implementering/SW/PSoC/Parameters/Parameters/parameter_test.c
@@ -1,8 +1,17 @@
+#pragma once
+
 #include "parameters.h"
+
+
 
 int main()
 {
-	parameters myStruct;
+	//parameters myStruct;
+	//parameters * Ptr_ = &myStruct;
+
+	//parameters_init(Ptr_);
+
+	//std::cout << Ptr_->active_ << " " << Ptr_->humidity_ << " " << Ptr_->temperature_ << std::endl;
 
 
 	while (1);
